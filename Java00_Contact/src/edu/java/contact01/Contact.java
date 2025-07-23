deb8706bab70d7b1d41e23cd76a0f72786983850
Ubokchi <105459234+Ubokchi@users.noreply.github.com>
2025-07-23 16:40:26 +0900

    0.1 - 메소드화

--- Java00_Contact/src/edu/java/contact01/Contact.java
+++ Java00_Contact/src/edu/java/contact01/Contact.java
@@ -1,44 +1,115 @@
-	package edu.java.contact01;
-	
-	public class Contact {
-		private String name;
-		private String phone;
-		private String email;
-		
-		public Contact() {}
-		
-		public Contact(String name, String phone, String email) {
-			this.name = name;
-			this.phone = phone;
-			this.email = email;
-		}
-		
-		public String toString() {
-		    return "Contact [name=" + name + ", phone=" + phone + ", email=" + email + "]";
-		}
-	
-		public String getName() {
-			return name;
-		}
-	
-		public void setName(String name) {
-			this.name = name;
-		}
-	
-		public String getPhone() {
-			return phone;
-		}
-	
-		public void setPhone(String phone) {
-			this.phone = phone;
-		}
-	
-		public String getEmail() {
-			return email;
-		}
-	
-		public void setEmail(String email) {
-			this.email = email;
-		}
-		
+package edu.java.contact01;
+
+import java.util.Scanner;
+
+public class Contact {
+	private String name;
+	private String phone;
+	private String email;
+
+	public Contact() {}
+
+	public Contact(String name, String phone, String email) {
+		this.name = name;
+		this.phone = phone;
+		this.email = email;
 	}
+
+	@Override
+	public String toString() {
+		return "Contact [name=" + name + ", phone=" + phone + ", email=" + email + "]";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	// 아래는 Contact 배열을 다루기 위한 static 메서드들
+
+	public static int insert(Contact[] list, int size, Scanner sc) {
+		System.out.println("-------------");
+		System.out.println("연락처 등록 메뉴");
+		System.out.println("-------------");
+		System.out.println("이름 입력> ");
+		String name = sc.next();
+		System.out.println("전화번호 입력> ");
+		String phone = sc.next();
+		System.out.println("이메일 입력> ");
+		String email = sc.next();
+
+		list[size] = new Contact(name, phone, email);
+		System.out.println("등록된 연락처 개수: " + (size + 1));
+		System.out.println("연락처 등록 완료!");
+		return size + 1;
+	}
+
+	public static void searchAll(Contact[] list, int size) {
+		System.out.println("연락처 개수: " + size);
+		for (int i = 0; i < size; i++) {
+			System.out.println(list[i]);
+		}
+	}
+
+	public static void searchByIndex(Contact[] list, int size, Scanner sc) {
+		System.out.println("검색할 인덱스 입력> ");
+		int index = sc.nextInt();
+		if (index < 0 || index >= size) {
+			System.out.println("잘못된 인덱스입니다.");
+			return;
+		}
+		System.out.println(list[index]);
+	}
+
+	public static void edit(Contact[] list, int size, Scanner sc) {
+		System.out.println("수정할 인덱스 입력> ");
+		int index = sc.nextInt();
+		if (index < 0 || index >= size) {
+			System.out.println("잘못된 인덱스입니다.");
+			return;
+		}
+		System.out.println("이름 입력> ");
+		list[index].setName(sc.next());
+		System.out.println("전화번호 입력> ");
+		list[index].setPhone(sc.next());
+		System.out.println("이메일 입력> ");
+		list[index].setEmail(sc.next());
+
+		System.out.println("연락처 수정 완료!");
+		System.out.println(list[index]);
+	}
+
+	public static int delete(Contact[] list, int size, Scanner sc) {
+		System.out.println("삭제할 인덱스 입력> ");
+		int index = sc.nextInt();
+		if (index < 0 || index >= size) {
+			System.out.println("잘못된 인덱스입니다.");
+			return size;
+		}
+		for (int i = index; i < size - 1; i++) {
+			list[i] = list[i + 1];
+		}
+		list[size - 1] = null;
+		System.out.println("연락처 삭제 완료!");
+		return size - 1;
+	}
+}
--- Java00_Contact/src/edu/java/contact01/ContactMain01.java
+++ Java00_Contact/src/edu/java/contact01/ContactMain01.java
@@ -9,105 +9,43 @@ public class ContactMain01 {
 	public static final int MENU_SEARCHBYINDEX = 3;
 	public static final int MENU_EDIT = 4;
 	public static final int MENU_DELETE = 5;
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Contact[] list = new Contact[100];
 		int choice = -1;
 		int size = 0;
-		
+
 		while(choice != MENU_QUIT) {
 			System.out.println("--------------------------------------------------------");
 			System.out.println("1. 등록 | 2. 전체검색 | 3. 상세검색 | 4. 수정 | 5. 삭제 | 0. 종료");
 			System.out.println("--------------------------------------------------------");
 			System.out.println("선택>");
 			choice = sc.nextInt();
-			
-			switch(choice) {
+
+			switch (choice) {
 			case MENU_QUIT:
-				choice = MENU_QUIT;
 				System.out.println("프로그램 종료");
 				break;
 			case MENU_INSERT:
-				System.out.println("-------------");
-				System.out.println("연락처 등록 메뉴");
-				System.out.println("-------------");
-				System.out.println("이름 입력>");
-				String name = sc.next();
-				System.out.println("전화번호 입력>");
-				String phone = sc.next();
-				System.out.println("이메일 입력>");
-				String email = sc.next();
-				
-				list[size] = new Contact(name, phone, email);
-				size++;
-				
-				System.out.println("등록된 연락처 개수: " + size);
-				System.out.println("연락처 등록 완료!");
+				size = Contact.insert(list, size, sc);
 				break;
 			case MENU_SEARCHALL:
-				System.out.println("연락처 개수: " + size);
-				
-				for(int i = 0; i < size; i++) {
-					System.out.println(list[i].toString());
-				}
+				Contact.searchAll(list, size);
 				break;
 			case MENU_SEARCHBYINDEX:
-				System.out.println("검색할 인덱스 입력>");
-				int search = sc.nextInt();
-				
-				if (search < 0 || search >= size) {
-					System.out.println("잘못된 인덱스입니다.");
-					break;
-				}
-				
-				System.out.println(list[search].toString());
+				Contact.searchByIndex(list, size, sc);
 				break;
 			case MENU_EDIT:
-				System.out.println("수정할 인덱스 입력>");
-				int edit = sc.nextInt();
-				
-				if (edit < 0 || edit >= size) {
-					System.out.println("잘못된 인덱스입니다.");
-					break;
-				}
-				
-				System.out.println("이름 입력>");
-				list[edit].setName(sc.next());
-				System.out.println("전화번호 입력>");
-				list[edit].setPhone(sc.next());
-				System.out.println("이메일 입력>");
-				list[edit].setEmail(sc.next());
-				
-				System.out.println("연락처 수정 완료!");
-				System.out.println(list[edit].toString());
+				Contact.edit(list, size, sc);
 				break;
-				
 			case MENU_DELETE:
-				System.out.println("삭제할 인덱스 입력>");
-				int delete = sc.nextInt();
-				
-				if (delete < 0 || delete >= size) {
-					System.out.println("잘못된 인덱스입니다.");
-					break;
-				}
-				
-				for (int i = delete; i < size - 1; i++) {
-					list[i] = list[i + 1];
-				}
-				
-				list[size - 1] = null;
-				size--;
-				
-				System.out.println("연락처 삭제 완료!");
+				size = Contact.delete(list, size, sc);
 				break;
-				
 			default:
-				System.out.println("0~4 중에 선택하세요.");
+				System.out.println("0~5 중에 선택하세요.");
 			}
-			
+
 		}
-
 	}
-
 }
