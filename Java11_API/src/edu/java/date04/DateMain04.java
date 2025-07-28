dce7298e2d8df8564d92310d804a75ce8300be71
Ubokchi <105459234+Ubokchi@users.noreply.github.com>
2025-07-28 11:20:54 +0900

    API

---
+++ Java11_API/src/edu/java/date04/DateMain04.java
@@ -0,0 +1,23 @@
+package edu.java.date04;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateMain04 {
+
+	public static void main(String[] args) {
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		String formattedDateTime = now.format(formatter);
+		System.out.println(formattedDateTime);
+	} // end main()
+
+} // end DateMain04
+
+
+
+
+
+
+
+
