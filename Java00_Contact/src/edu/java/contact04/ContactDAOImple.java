56dc66137b2fb1998a460798b4c7ac89fe2687f9
Ubokchi <105459234+Ubokchi@users.noreply.github.com>
2025-07-31 18:12:49 +0900

    연락처 0.4.2 최종본

--- Java00_Contact/src/edu/java/contact04/ContactDAOImple.java
+++ Java00_Contact/src/edu/java/contact04/ContactDAOImple.java
@@ -4,6 +4,7 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,124 +13,151 @@ import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 
-public class ContactDAOImple implements ContactDAO{
-
+// DAO(Data Access Object) :
+// - 데이터의 전송을 담당하는 역할
+public class ContactDAOImple implements ContactDAO {
+	
 	// 싱글톤 디자인 패턴 적용
+	
 	// 1. private static 자기 자신 타입의 변수 선언
 	private static ContactDAOImple instance = null;
-
+	
 	// 2. private 생성자
-	private ContactDAOImple() {}
-
-	// 3. public static 메소드
+	private ContactDAOImple() {
+		initDataDir();
+		initDataFile();
+	}
+	
+	// 3. public static 메소드 - 인스턴스를 리턴하는 메소드 구현
 	public static ContactDAOImple getInstance() {
 		if(instance == null) {
 			instance = new ContactDAOImple();
 		}
 		return instance;
-	} 
-
-	private final int MAX = 100; 
-	private ArrayList<ContactVO> list = new ArrayList<ContactVO>(); 
-
+	}
+	
+	private ArrayList<ContactVO> list = new ArrayList<>(); // 연락처 정보를 저장할 리스트
+	
 	// 데이터를 저장할 폴더와 파일 이름 정의
 	private static final String DATA_DIR = "data";
 	private static final String DATA_FILE = "contact.data";
-	private static final String FILE_PATH = DATA_DIR + File.separator + DATA_FILE;
-
+	
 	// data 폴더의 contact.data 파일을 관리할 File 객체 선언
-	private File dataDir; // 폴더
+	private File dataDir; // 경로
 	private File dataFile; // 파일
-
-	public int getSize() {
-		return list.size();
-	}
-
-	// data 폴더가 있는지 검사하고, 없으면 생성하는 메소드
+	
+	// TODO : data 폴더가 있는지 검사하고, 없으면 생성하는 함수
 	private void initDataDir() {
+		System.out.println("initDataDir()");
 		dataDir = new File(DATA_DIR);
-	    if (!dataDir.exists()) {
-	    	dataDir.mkdirs();
-	    }
-	} 
-
-	// 데이터 파일이 존재하는지 엄사하고, 
+		System.out.println("폴더 경로 : " + dataDir.getPath());
+		System.out.println("절대 경로 : " + dataDir.getAbsolutePath());
+		
+		if(!dataDir.exists()) {
+			if(dataDir.mkdirs()) {
+				System.out.println("<폴더 생성 성공>");
+			} else {
+				System.out.println("<폴더 생성 실패>");
+			}
+		} else { // 폴더가 있으면
+			System.out.println("<폴더가 이미 존재>");
+		}
+		
+	}
+	
+	// TODO : 데이터 파일이 존재하는지 검사하고,
 	// 없는 경우 - 새로운 데이터 파일 생성
 	// 있는 경우 - 기존 파일에서 데이터를 읽어서 ArrayList에 추가
 	private void initDataFile() {
-	    initDataDir();
-
-	    dataFile = new File(FILE_PATH);
-	    if (!dataFile.exists()) {
-	        try {
-	            dataFile.createNewFile();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	    }
-
-	    readDataFromFile();
+		System.out.println("initDataFile()");
+		String filePath = DATA_DIR + File.separator + DATA_FILE;
+		dataFile = new File(filePath);
+		
+		if(!dataFile.exists()) {
+			System.out.println("파일이 없습니다.");
+			try {
+				if(dataFile.createNewFile()) {
+					System.out.println("파일 생성 성공");
+				} else {
+					System.out.println("파일 생성 실패");
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("파일이 이미 존재합니다.");
+			System.out.println("데이터 크기 : " + dataFile.length());
+			if(dataFile.length() != 0) {
+				readDataFromFile();				
+			}
+		}
 	}
-
-	// 멤버 변수 list 객체를 data/contact.data 파일에 저장(쓰기)
+	
+	// TODO : 멤버 변수 list 객체를 data\contact.data 파일에 저장(쓰기)
 	private void writeDataToFile() {
 		OutputStream out = null;
 		BufferedOutputStream bout = null;
 		ObjectOutputStream oout = null;
-
+		
 		try {
-			out = new FileOutputStream("data/contact.data");
+			out = new FileOutputStream(dataFile);
 			bout = new BufferedOutputStream(out);
 			oout = new ObjectOutputStream(bout);
-
+			
 			oout.writeObject(list);
+			
+			System.out.println("데이터 저장 완료");
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			try {
 				oout.close();
 			} catch (IOException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-
-	}
-
-	// data/contact.data 파일에서 ArrayList 객체를 읽어와서
-	//멤버 변수 list에 저장(읽기)
+		
+	} // end writeDataToFile()
+	
+	// TODO : data\contact.data 파일에서 ArrayList 객체를 읽어와서
+	// 멤버 변수 list에 저장(읽기)
 	private void readDataFromFile() {
+		System.out.println("readDataFromFile()");
 		InputStream in = null;
 		BufferedInputStream bin = null;
 		ObjectInputStream oin = null;
-
+		
 		try {
-			in = new FileInputStream("data/contact.data");
+			in = new FileInputStream(dataFile);
 			bin = new BufferedInputStream(in);
 			oin = new ObjectInputStream(bin);
-
+			
 			list = (ArrayList<ContactVO>) oin.readObject();
-
+			System.out.println("list 데이터 읽기 완료");
 		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
 			try {
 				oin.close();
 			} catch (IOException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
+		
+	} // end readDataFromFile()
+	
+	public int getSize() {
+		return list.size();
 	}
-
 
 	@Override
 	public int insert(ContactVO vo) {
-		initDataFile();
-		if(list.size() == MAX) {
-			return 0;
-		} else {
-			list.add(vo);
-			writeDataToFile();
-		}
+		list.add(vo);
+		writeDataToFile();
 		return 1; // 0 : 실패, 1 : 성공
 	}
 
@@ -145,7 +173,8 @@ public class ContactDAOImple implements ContactDAO{
 
 	@Override
 	public int update(int index, ContactVO vo) {
-		list.set(index, vo);
+		list.get(index).setPhone(vo.getPhone());
+		list.get(index).setEmail(vo.getEmail());
 		writeDataToFile();
 		return 1;
 	}
